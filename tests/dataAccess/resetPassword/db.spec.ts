bc5b6d38bfbda220dc4b1bca4b45ae65ce451791
Yuichi Hagio <yhagio87@gmail.com>
2020-03-12 00:06:01 -0400

    tests templates

---
+++ tests/dataAccess/resetPassword/db.spec.ts
@@ -0,0 +1,18 @@
+import sinon from 'sinon';
+// import { expect } from 'chai';
+
+describe('Service', () => {
+  const sandbox = sinon.createSandbox();
+
+  beforeEach(() => {
+
+  });
+
+  afterEach(() => {
+    sandbox.reset();
+  });
+
+  describe('...', () => {
+    it('...');
+  });
+});

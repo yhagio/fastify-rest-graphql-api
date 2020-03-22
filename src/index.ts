673902a92338d73bb81e018baeaea79ff2782b74
Yuichi Hagio <yhagio87@gmail.com>
2020-03-22 16:13:49 -0400

    address added to listen

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import app from './app';
 
 const start = async () => {
   try {
-    const address = await app.listen(process.env.PORT || (3000 as any));
+    const address = await app.listen(process.env.PORT || (3000 as any), '0.0.0.0');
     app.log.info(`server listening on ${address}`);
   } catch (err) {
     app.log.error(err);

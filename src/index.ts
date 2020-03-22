c3e0785971a9f3b83a8297710946cca352696c01
Yuichi Hagio <yhagio87@gmail.com>
2020-03-22 15:22:49 -0400

    port listening

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { AddressInfo } from 'net';
 
 const start = async () => {
   try {
-    await app.listen(3000);
+    await app.listen(process.env.PORT || process.env.APP_PORT || (3000 as any));
     const server = app.server.address() as AddressInfo;
     app.log.info(`server listening on ${server.address}:${server.port}`);
   } catch (err) {
